b46b83ee33adbe24568c3bb650a0dbd1ee1354d8
Fernando Prange <fpmenechini@gmail.com>
2025-08-15 00:31:04 -0300

    primeiro commit

---
+++ create_parquet.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+data = {'product_id': [1, 2, 3, 4, 1, 3],
+        'product_name': ['Laptop', 'Mouse', 'Keyboard', 'Monitor', 'Laptop', 'Keyboard'],
+        'price': [1200, 25, 75, 300, 1250, 70]}
+df = pd.DataFrame(data)
+
+df.to_parquet('products.parquet', engine='pyarrow')
---
+++ main.py
@@ -0,0 +1,33 @@
+import duckdb
+
+
+# Utilizar o duckdb em memória
+
+con_memory = duckdb.connect(database=':memory:', read_only=False)
+
+con_memory.execute("SELECT * FROM './products.csv'").fetchdf()
+con_memory.execute("SELECT * FROM './products.parquet'").fetchdf()
+
+
+# Utilizar com um .db
+
+db_file = 'my_duckdb.db'
+con_file = duckdb.connect(database=db_file, read_only=False)
+
+con_file.execute("CREATE OR REPLACE TABLE products_csv AS SELECT * FROM './products.csv'")
+con_file.execute("CREATE OR REPLACE TABLE products_parquet AS SELECT * FROM './products.parquet'")
+
+aggregation_query = """
+    SELECT 
+        product_name, 
+        COUNT(product_id) as quantity, 
+        AVG(price) as average_price
+    FROM products_csv
+    GROUP BY product_name
+    ORDER BY product_name
+"""
+
+con_file.execute(f"CREATE OR REPLACE TABLE aggregated_products AS ({aggregation_query})")
+
+con_memory.close()
+con_file.close()
